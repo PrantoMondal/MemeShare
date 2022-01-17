9e572315d6d0b561d59bfebdd1867830561c7d11
PrantoMondal <prantomondal517@gmail.com>
2022-01-18 00:57:19 +0600

    UI added

---
+++ app/src/main/java/com/prantokm/memeshare/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.prantokm.memeshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun shareMeme(view:View){
+
+    }
+    fun nextMeme(view: View){
+
+    }
+}
