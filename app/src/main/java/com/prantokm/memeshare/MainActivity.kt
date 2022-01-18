8cf142c9d27b91955abb2abfb6d42c23d660b9ee
PrantoMondal <prantomondal517@gmail.com>
2022-01-19 02:08:33 +0600

    Load meme done

--- app/src/main/java/com/prantokm/memeshare/MainActivity.kt
+++ app/src/main/java/com/prantokm/memeshare/MainActivity.kt
@@ -9,6 +9,8 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,10 +25,11 @@ class MainActivity : AppCompatActivity() {
         val url = "https://www.google.com"
 
         // Request a string response from the provided URL.
-        val JsonObjectRequest = JsonObjectRequest(
+        val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
             Response.Listener { response ->
                 val url = response.getString("url")
+                Glide.with(this).load(url).into(memeImageView)
 
             },
             Response.ErrorListener {
@@ -34,7 +37,7 @@ class MainActivity : AppCompatActivity() {
             })
 
 // Add the request to the RequestQueue.
-        queue.add(JsonObjectRequest)
+        queue.add(jsonObjectRequest)
     }
     fun shareMeme(view:View){
 
