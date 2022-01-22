f33ee10b7a9c43c6f1d4b846c53f79ae8d11fde7
PrantoMondal <prantomondal517@gmail.com>
2022-01-22 14:52:34 +0600

    Singleton class added

--- app/src/main/java/com/prantokm/memeshare/MainActivity.kt
+++ app/src/main/java/com/prantokm/memeshare/MainActivity.kt
@@ -4,19 +4,17 @@ import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,8 +27,9 @@ class MainActivity : AppCompatActivity() {
     }
     private fun loadMeme(){
         progressBar.visibility = View.VISIBLE
+//        nextBtn.isEnabled = false
+//        shareBtn.isEnabled = false
         // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
 
         // Request a string response from the provided URL.
@@ -68,7 +67,7 @@ class MainActivity : AppCompatActivity() {
             })
 
 // Add the request to the RequestQueue.
-        queue.add(jsonObjectRequest)
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
     fun shareMeme(view:View){
         val intent = Intent(Intent.ACTION_SEND)
